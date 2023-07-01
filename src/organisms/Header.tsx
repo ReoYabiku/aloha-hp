b4c6d60f156a0d80953e5e9532f580c85a23c1c4
ReoYabiku <alcremikyu.z@gmail.com>
2023-07-01 21:10:06 +0900

    縦を考慮しない配置

--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -1,13 +1,25 @@
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
   return (
-    <>
-      <Link to={'/introduction'}>団体紹介</Link>
-      <Link to={'/members'}>メンバー紹介</Link>
-      <Link to={'/documents'}>資料一覧</Link>
-      <Link to={'/activities'}>活動実績</Link>
-    </>
+    <header>
+      <div className='header-logo'>
+        <div className='header-logo-img-wrapper'>
+          <img src="ALOHA_logo.png" alt="ALOHAのロゴ" width={40} height={40} />
+        </div>
+        <div>
+          <h1 className='header-title'>ALOHA</h1>
+          <p className='header-subtitle'>沖縄から東大を、日常に</p>
+        </div>
+      </div>
+      <div className='header-nav'>
+        <Link to={'/introduction'}>団体紹介</Link>
+        <Link to={'/members'}>メンバー紹介</Link>
+        <Link to={'/documents'}>資料一覧</Link>
+        <Link to={'/activities'}>活動実績</Link>
+      </div>
+    </header>
   );
 }
 
