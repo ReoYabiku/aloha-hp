3afb1625ef464f9865ecc7a8001ce967ffc7d528
ReoYabiku <alcremikyu.z@gmail.com>
2023-07-04 15:26:20 +0900

    header

--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -1,23 +1,23 @@
 import { Link } from 'react-router-dom';
-import './Header.css';
+import style from './Header.module.css';
 
 const Header = () => {
   return (
     <header>
-      <div className='header-logo'>
-        <div className='header-logo-img-wrapper'>
-          <img src="ALOHA_logo.png" alt="ALOHAのロゴ" width={40} height={40} />
+      <Link to={'/'} className={style.logo}>
+        <div className={style.logo_img_wrapper}>
+          <img src="ALOHA_logo.png" alt="ALOHAのロゴ" width={45} height={45} />
         </div>
         <div>
-          <h1 className='header-title'>ALOHA</h1>
-          <p className='header-subtitle'>沖縄から東大を、日常に</p>
+          <h1 className={style.title}>ALOHA</h1>
+          <p className={style.subtitle}>沖縄から東大を、日常に</p>
         </div>
-      </div>
-      <div className='header-nav'>
-        <Link to={'/introduction'}>団体紹介</Link>
-        <Link to={'/members'}>メンバー紹介</Link>
-        <Link to={'/documents'}>資料一覧</Link>
-        <Link to={'/activities'}>活動実績</Link>
+      </Link>
+      <div className={style.nav}>
+        <Link to={'/introduction'} className={style.other_pages}>団体紹介</Link>
+        <Link to={'/members'} className={style.other_pages}>メンバー紹介</Link>
+        <Link to={'/documents'} className={style.other_pages}>資料一覧</Link>
+        <Link to={'/activities'} className={style.other_pages}>活動実績</Link>
       </div>
     </header>
   );
